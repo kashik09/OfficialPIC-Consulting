b203fcd121ec8ceab5a91370f8edbe352be6eb33
Kashi Kweyu <kashiku789@gmail.com>
2025-03-22 17:12:20 +0300

    Delete form const

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -59,7 +59,6 @@ if ('ontouchstart' in window || navigator.maxTouchPoints) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("#contactForm");
     const phoneInputField = document.querySelector("#floatingPhone");
     const emailInput = document.getElementById("floatingEmail");
     const messageInput = document.getElementById("floatingMessage");
